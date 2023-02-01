630552be7a848c948ea5dce21754124b1494b5e7
engageintellect <jc9361@gmail.com>
2023-02-01 00:01:52 -0800

    adding minimal option

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,14 +10,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Casual" | "Funny";
+export type VibeType = "Professional" | "Casual" | "Funny" | "Minimal";
 
 interface DropDownProps {
   vibe: VibeType;
   setVibe: (vibe: VibeType) => void;
 }
 
-let vibes: VibeType[] = ["Professional", "Casual", "Funny"];
+let vibes: VibeType[] = ["Professional", "Casual", "Funny", "Minimal"];
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
